085e23560ff2dce44ab596174928497d46a0ddf7
Paulo Moreira <phmoreira37@gmail.com>
2023-09-04 21:51:21 -0300

    refact: SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  movieGenres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar(props: GenreResponseProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.movieGenres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
